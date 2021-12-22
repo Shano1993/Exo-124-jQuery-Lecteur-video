56432c67f9ecd4d8b9854c129b0efad0508ddfff
Shano1993 <stefan.hanotiau@gmail.com>
2021-12-22 11:24:44 +0100

    player

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+let toggle = true;
+
+$('.toggle').click(function () {
+    if (toggle) {
+        $(this).html("||");
+        toggle = false;
+        player.get(0).play();
+    }
+    else {
+        $(this).html("►");
+        toggle = true;
+        player.get(0).pause();
+    }
+});
+
+$('button').click(function () {
+    switch ($(this).attr("data-skip")) {
+        case "-10":
+            player[0].currentTime -= 10;
+            break;
+        case "25":
+            player[0].currentTime += 25;
+            break;
+    }
+})
+
+$('#volume').on("change", function () {
+    player[0].volume = this.value;
+})
+
